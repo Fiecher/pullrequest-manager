9dd0061654b2fccf15e24ead36559471f24fff67
Fiecher <stepanbelebezev@gmail.com>
2025-11-21 18:36:31 +0300

    feat: add main.go

---
+++ cmd/pullrequest-manager/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"pullrequest-manager/internal/application/services"
+	"pullrequest-manager/internal/infrastructure/database/pg"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	} else {
+		port = ":" + port
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		log.Fatalf("Failed to connect to database:", err)
+	}
+	defer pool.Close()
+
+	prRepo := pg.NewPullRequestRepository(pool)
+	statusRepo := pg.NewStatusRepository(pool)
+	teamRepo := pg.NewTeamRepository(pool)
+	userRepo := pg.NewUserRepository(pool)
+
+	teamService, err := services.NewDefaultPullRequestService(userRepo, prRepo, teamRepo, statusRepo)
+	_ = teamService
+
+}
